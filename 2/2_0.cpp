ba2025d7a240ead3c6a0299fe619639e7744a329
Matej Vasilevski <matej.vasilevski@gmail.com>
2018-07-11 00:00:03 +0200

    Add solution to exercise 2.0
    Source code is polluted with "unsigned" definitions to be 999 % correct
    (also the compiler flags are -Wextra -Wall --pedantic-errors, so it
    complaints about literally _everything_ :D and I'm too much a lazy ass
    to change it)

--- 2/2_0.cpp
+++ 2/2_0.cpp
@@ -1,2 +1,38 @@
-// new program hmmmm
-// dem diffs tho
+#include <iostream>
+
+int main()
+{
+    const unsigned int padding = 12;
+    const unsigned int rows = 2*padding + 3;
+
+    std::string name;
+    std::cin >> name;
+
+    const std::string greeting = "Hi, " + name;
+    const std::string::size_type greetSize = greeting.size();
+    const unsigned int cols = greetSize + 2*padding + 2;
+
+    std::cout << std::endl;
+
+    for (unsigned int row = 0; row < rows; ++row)
+    {
+        std::string::size_type col = 0;
+        while (col < cols)
+        {
+            if (row == 0 || row == rows -1 || col == 0 || col == cols - 1){
+                std::cout << "*";
+                col++;
+            }
+            else if (row == padding + 1 && col == padding + 1){
+                std::cout << greeting;
+                col += greetSize;
+            }
+            else {
+                std::cout << " ";
+                col++;
+            }
+        } 
+        std::cout << std::endl;
+    }
+}
+
