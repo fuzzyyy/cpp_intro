b2206f96c8ce9265e49b9d3d21ddb1fdcfb48baf
Matej Vasilevski <matej.vasilevski@gmail.com>
2018-08-26 23:16:47 +0200

    Add solution to exercise 8.4

---
+++ 8/8_4.cpp
@@ -0,0 +1,16 @@
+#include "algs.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int a = 0;
+    int b = 1;
+    cout << a << " " << b << "\n";
+    my_swap(a, b);
+    cout << a << " " << b << "\n";
+    return 0;
+}
